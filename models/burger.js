c6eb4ed99c53e4e405451f2bd818afb323c7e618
Alex Dollard <alexdollard141@gmail.com>
2020-02-06 22:40:55 -0600

    cleaned up code & made minor style adjustments

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,5 @@
-// // // Sequelize (capital) references the standard library
-// const {Sequelize ,Datatypes} = require("sequelize");
-// // // sequelize (lowercase) references our connection to the DB.
+
 let sequelize = require("../config/connection.js");
-
-
-// let Burger = sequelize.define("Burger", {
-//     name: Sequelize.STRING,
-//     isMunched: Sequelize.BOOLEAN
-// })
-
-// Burger.sync();
-
-// module.exports = Burger;
-
 
 module.exports = function(sequelize, Datatypes){
     let Burger = sequelize.define("Burger", {
--- public/js/index.js
+++ public/js/index.js
@@ -14,59 +14,10 @@ $("#add-button").on("click", async function(event){
     });
     await $("#burger_form").val("");
     location.reload();
-
-    // getBurgers();
 }
 )
 
-
-
-//this function sends a get request for all burgers and sorts them according to isMunched T or F
-//  function getBurgers() {
-//     $.get("/api/burger",  function(burger){
-//     //clear the current list 
-//     $("#to_munch_list").empty();
-//     $("#munched_list").empty();
-//     console.log(burger);
-//     //loop through responses and append a div for each burger
-//      for(let i=0; i<burger.length; i++){
-//         //if burger is not munched, create and append to left column
-//         if(!burger[i].isMunched) {
-//         let new_div = $("<div>");
- 
-//         new_div.attr("id", "burger-well-" + burger[i].id);
-//         let munch_button = document.createElement("button")
-//         munch_button.textContent = "Munch";
-//         munch_button.setAttribute("class","munch_button")
-//         munch_button.setAttribute("id", burger[i].id);
-
-//         $("#to_munch_list").append(new_div);
-//         $("#burger-well-" + burger[i].id).append("<p>" + burger[i].name + "<p>");
-//         $("#burger-well-" + burger[i].id).append(munch_button)
-//         }
-//         //if burger is munched, create and append to right column
-//          else {
-//             let new_div = $("<div>");
-     
-//         new_div.attr("id", "eaten-well-" + burger[i].id);
-//         let poop_button = document.createElement("button")
-//         poop_button.textContent = "Poop the burger";
-//         poop_button.setAttribute("class","poop_button")
-//         poop_button.setAttribute("id", burger[i].id);
-
-//         $("#munched_list").append(new_div);
-//         $("#eaten-well-" + burger[i].id).append("<p>" + burger[i].name + "<p>");
-//         $("#eaten-well-" + burger[i].id).append(poop_button)
-//         }
-//     }
-//     //call click-listener functions for new buttons
-//     devourBurger();
-//     deleteBurger();
-// })
-// }
-
-//on click, will send ajax put to change isMunched to true, and re-render buger list
-
+//on click, will change isMunched to true and reload the page
  $(".munch_button").on("click", function(event){
     event.preventDefault();
     console.log("HELLOOOOOOO")
@@ -90,7 +41,6 @@ $("#add-button").on("click", async function(event){
 
 
 //on click, will send ajax delete request
-
       $(".poop_button").on("click", async function(event) {
         event.preventDefault();
         console.log("GOODBYYYEEE burger number: " + this.id);
@@ -109,7 +59,3 @@ $("#add-button").on("click", async function(event){
         })
     });
 
-
-
-
-// getBurgers();
